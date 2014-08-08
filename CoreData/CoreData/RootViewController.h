a6acd053b118a7b531f760d53a765aa0a908d5ba
TakanoriMatsumoto <matscube@TakanoriMatsumoto-no-MacBook-Pro-2.local>
2014-08-09 01:59:50 +0900

    Initialize project.

---
+++ CoreData/CoreData/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  CoreData
+//
+//  Created by TakanoriMatsumoto on 2014/08/09.
+//  Copyright (c) 2014年 TakanoriMatsumoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
